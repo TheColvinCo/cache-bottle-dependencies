148331e387ea3c4f1b77c67253e49a569d0d86da
jlopezxs <jlopezxs@gmail.com>
2020-08-16 13:08:44 +0200

    add dummy test

---
+++ tests/main.spec.ts
@@ -0,0 +1,7 @@
+describe("cache-bottle-dependencies", () => {
+  describe("when you call it", () => {
+    it("should return the proper result", () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
